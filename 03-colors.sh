7c330758b8ab0894035fc761c80a23058a671e77
sarvan <msaravanakumar312@gmail.com>
2023-08-01 19:46:32 +0530

    Auto push

--- 03-colors.sh
+++ 03-colors.sh
@@ -2,13 +2,11 @@
 
 
 ######
-...........
 #Color	Foreground Code	Background Code
 #Red	31	41
 #Green	32	42
 #Yellow	33	43
 #Blue	34	44
-...............
 #####
 
 echo -e "\e[33 I am printing the yellow color \e[0m"

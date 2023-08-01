e121a6d30096a479712df55603bcd98198c5f603
sarvan <msaravanakumar312@gmail.com>
2023-08-01 19:45:31 +0530

    Auto push

---
+++ 03-colors.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+######
+...........
+#Color	Foreground Code	Background Code
+#Red	31	41
+#Green	32	42
+#Yellow	33	43
+#Blue	34	44
+...............
+#####
+
+echo -e "\e[33 I am printing the yellow color \e[0m"
+echo -e "\e[34 I am printing the blue color \e[0m"

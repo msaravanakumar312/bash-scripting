d25f84ea9f351bd0a36fff509744977c9d2720ba
sarvan <msaravanakumar312@gmail.com>
2023-08-02 13:21:29 +0530

    Auto push

--- 06-specialvariables.sh
+++ 06-specialvariables.sh
@@ -10,3 +10,19 @@ echo "Excuted script name is : $0"
 echo "Name of recently launched rocket in india is $1"
 echo "Popular EV vechicle in state is $2"
 echo "Current topic is $3"
+
+
+# bash arg1 arg2 arg3 arg4 arg5 arg6 arg7 arg8 arg9 arg10
+#bash.scriptname.sh 100 200 300 (like this you can supply maximum of 9 variables from the command line)
+
+# $* : is going to print the used variables
+# $@ : is going to print the used variables
+# $$ : is going to print the PID of the current process
+# $# : is going to print the number of argument
+# $? : is going to print the exit code of the last command
+
+echo $0
+echo $@
+echo $$
+echo $#
+echo $?

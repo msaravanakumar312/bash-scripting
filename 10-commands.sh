c574787f1bb68f15bd1d1c1287207ca3eceb96f0
sarvan <msaravanakumar312@gmail.com>
2023-08-02 14:06:29 +0530

    Auto push

---
+++ 10-commands.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+There are four types of command available :
+
+# 1> Binary    : /bin , /sbin
+# 2> Aliases   : Aliases are shortcuts , alias netstat-tulpn*
+# 3> Shell built in commands 
+# 4> Functions  : Funtions is nothing but a set of command that can be written in a sequence and can be called n number of per
+
+
+sample() {
+    echo hai from the sample function
+    echo sample funtion is completed
+}
+
+sample

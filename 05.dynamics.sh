3ab995bee2a83872e605a6b2a768ae195771e130
sarvan <msaravanakumar312@gmail.com>
2023-08-02 12:51:19 +0530

    Auto push

--- 05.dynamics.sh
+++ 05.dynamics.sh
@@ -3,5 +3,5 @@
 DATE="$(date +%F)"
 SESSION_COUNT=$(who | wc -l)
 
-echo "Today date is /e[32m $DATE /e[0m"
-echo "Total number of active session count is /e[32m $SESSION_COUNT /e[0m"
+echo "Today date is \e[32m $DATE \e[0m"
+echo "Total number of active session count is \e[32m $SESSION_COUNT \e[0m"

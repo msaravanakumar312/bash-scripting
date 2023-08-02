8f06049aee7881908f442030580d70ce3a2f6ec7
sarvan <msaravanakumar312@gmail.com>
2023-08-02 13:12:29 +0530

    Auto push

---
+++ 06-specialvariables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#special variables gives special propeities to your variables.
+
+#Here are few of the special variables : $0 to $9 , $? , $#, $$ , $@
+
+echo $0        ## $0 prints the script name you're excuting
+echo "Excuted script name is : $0"
+
+echo "Name of recently launched rocket in india is $1"
+echo "Popular EV vechicle in state is $2"
+echo "Current topic is $3"

de9b83d0a3dbad6ba1b4457b510c3d2f9c790946
sarvan <msaravanakumar312@gmail.com>
2023-08-04 11:53:16 +0530

    Auto push

--- 15-Ifelse-condition.sh
+++ 15-Ifelse-condition.sh
@@ -8,9 +8,19 @@ if [ "$ACTION" == "start" ]; then
     echo -e "\e[32m Starting payment service \e[0m"
     exit 0
 
+elif [ "ACTION" == "stop" ]; then
+
+    echo -e "\e[31m stop payment service \e[0m"
+    exit 1
+
+elif [ "ACTION" == "restart" ]; then
+
+    echo -e "\e[33m restart payment service \e[0m"
+    exit 2
 else
 
-    echo -e "\e[32m Valid option is start only \e[0m "
-    exit 1
+    echo -e "\e[32m Valid option are start or stop or restart \e[0m "
+    echo -e "\e[32m Example usage \e[0m :/n /t bash scripting name stop"
+    exit 3
 
 fi

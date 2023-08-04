96dca628bdbeb7602a446105911e343b3bb9a24e
sarvan <msaravanakumar312@gmail.com>
2023-08-04 12:09:31 +0530

    Auto push

---
+++ 16-loop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+#loop are used to excuted something for certain number of times
+
+#They are two major types of loops:
+
+#1) for loop : when you know something to be excuted n number of times, we use for loops
+
+#2)while loop: when you don't know something to be excuted n number times, we use while loop.
+
+#for loop syntax
+
+for val in 10 20 30 40 50 ; do
+    echo "value for the loop is $val"
+done

b13e7cbe9e3aac3ea7ebebafe0b772c1e6cc55ea
sarvan <msaravanakumar312@gmail.com>
2023-08-07 21:21:45 +0530

    Auto push

--- roboshop/component/frontend.sh
+++ roboshop/component/frontend.sh
@@ -8,11 +8,6 @@ if [ $USER_ID -ne 0 ] ; then
     exit 1
 fi
 
-echo -e "\e[35m Configuring frontend \e[0m"
-echo -n "Installing frontend :"
-yum install nginx -y   &>> /tmp/frontend.log
-
-
 stat () {
 if [ $1 -eq 0 ] ; then
     echo -e "\e[32m Success \e[0m"
@@ -21,8 +16,12 @@ else
 fi
 }
 
-echo -n "starting nginx :"
+echo -e "\e[35m Configuring frontend \e[0m"
+echo -n "Installing frontend :"
+yum install nginx -y   &>> /tmp/frontend.log
+stat $?
 
+echo -n "starting nginx :"
 systemctl enable nginx    &>> /tmp/frontend.log
 systemctl start nginx     &>> /tmp/frontend.log
 stat $?
@@ -42,13 +41,6 @@ unzip /tmp/frontend.zip    &>> /tmp/frontend.log
 stat $?
 
 
-# yum install nginx -y
-# systemctl enable nginx
-# systemctl start nginx
-# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
-# cd /usr/share/nginx/html
-# rm -rf *
-# unzip /tmp/frontend.zip
 # mv frontend-main/* .
 # mv static/* .
 # rm -rf frontend-main README.md

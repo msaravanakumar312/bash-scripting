39774bc3ac3fa0530b1005df4ebfc950c2c47852
sarvan <msaravanakumar312@gmail.com>
2023-08-07 21:17:44 +0530

    Auto push

--- roboshop/component/frontend.sh
+++ roboshop/component/frontend.sh
@@ -9,37 +9,37 @@ if [ $USER_ID -ne 0 ] ; then
 fi
 
 echo -e "\e[35m Configuring frontend \e[0m"
-
-
 echo -n "Installing frontend :"
 yum install nginx -y   &>> /tmp/frontend.log
 
-if [ $? -eq 0 ] ; then
+
+stat () {
+if [ $1 -eq 0 ] ; then
     echo -e "\e[32m Success \e[0m"
 else
     echo -e "\e[31 Failure \e[0m"
 fi
+}
 
 echo -n "starting nginx :"
 
 systemctl enable nginx    &>> /tmp/frontend.log
 systemctl start nginx     &>> /tmp/frontend.log
-
-if [ $? -eq 0 ] ; then
-    echo -e "\e[32m Success \e[0m"
-else
-    echo -e "\e[31 Failure \e[0m"
-fi
+stat $?
 
 
 echo -n "Downloading the frontend component :"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+stat $?
 
-if [ $? -eq 0 ] ; then
-    echo -e "\e[32m Success \e[0m"
-else
-    echo -e "\e[31 Failure \e[0m"
-fi
+echo -n "Clean up the frontend :"
+cd /usr/share/nginx/html
+rm -rf *      &>> /tmp/frontend.log
+stat $?
+
+echo -n "Extracting the frontend :"
+unzip /tmp/frontend.zip    &>> /tmp/frontend.log
+stat $?
 
 
 # yum install nginx -y

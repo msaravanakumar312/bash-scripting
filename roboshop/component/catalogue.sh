28db85638cf3cb0dbbb3ba64a20f7f4f266133aa
sarvan <msaravanakumar312@gmail.com>
2023-08-08 14:21:43 +0530

    Auto push

--- roboshop/component/catalogue.sh
+++ roboshop/component/catalogue.sh
@@ -40,8 +40,8 @@ stat $?
 
 echo -n "Copying the ${COMPONENT} to ${APPUSER} home directory :"
 cd /home/${APPUSER}/
-rm -rf ${COMPONENT}      &>> ${LOGFILE}
-unzip -o /tmp/${COMPONENT}.zip  ${COMPONENT}
+rm -rf ${COMPONENT}             &>> ${LOGFILE}
+unzip -o /tmp/${COMPONENT}.zip  &>> ${LOGFILE}
 stat $?
 
 echo -n "Changing the ownership :"

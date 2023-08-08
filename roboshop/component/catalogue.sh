80eafebf5a33d42b33c911d172ed32043782dfa1
sarvan <msaravanakumar312@gmail.com>
2023-08-08 14:51:49 +0530

    Auto push

--- roboshop/component/catalogue.sh
+++ roboshop/component/catalogue.sh
@@ -49,6 +49,11 @@ mv ${COMPONENT}-main ${COMPONENT}
 chown -R ${APPUSER}:${APPUSER} /home/${APPUSER}/${COMPONENT}/
 stat $?
 
+echo -n "Generating the ${COMPONENT} artifacts :"
+cd /home/${APPUSER}/${COMPONENT}/
+npm install
+stat $?
+
 echo -n "Configuring ${COMPONENT} system file :"
 sed -ie 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/${APPUSER}/${COMPONENT}/systemd.service 
 mv /home/${APPUSER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service

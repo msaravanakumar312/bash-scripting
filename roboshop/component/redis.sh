61c81f24dcce2a0fca3bbe86b69d4894a01572ec
sarvan <msaravanakumar312@gmail.com>
2023-08-08 19:26:22 +0530

    Auto push

--- roboshop/component/redis.sh
+++ roboshop/component/redis.sh
@@ -33,6 +33,12 @@ sed -ie 's/127.0.0.1/0.0.0.0/g' /etc/${COMPONENT}.conf  &>> ${LOGFILE}
 sed -ie 's/127.0.0.1/0.0.0.0/g' /etc/${COMPONENT}/${COMPONENT}.conf  &>> ${LOGFILE}
 stat $?
 
+echo -n "Starting the ${COMPONENT} :"
+systemctl daemon-reload        &>> ${LOGFILE}
+systemctl enable ${COMPONENT}  &>> ${LOGFILE}
+systemctl start ${COMPONEnT}   &>> ${LOGFILE}
+stat $?
+
 
 
 

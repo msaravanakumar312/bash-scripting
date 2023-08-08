5e8d86a97d01f9f982b424b8d6cac755cf4ed00d
sarvan <msaravanakumar312@gmail.com>
2023-08-08 13:40:34 +0530

    Auto push

--- roboshop/component/catalogue.sh
+++ roboshop/component/catalogue.sh
@@ -35,7 +35,7 @@ if [ $? -ne 0 ] ; then
 fi
 
 echo -n "Downloading the ${COMPONENT} :"
-curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip"
+curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
 stat $?
 
 echo -n "Copying the ${COMPONENT} to ${APPUSER} home directory"

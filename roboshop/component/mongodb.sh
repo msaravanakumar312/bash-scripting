acf02177c1eaed085a2116896e63d7eb8f89ee29
sarvan <msaravanakumar312@gmail.com>
2023-08-08 20:22:52 +0530

    Auto push

--- roboshop/component/mongodb.sh
+++ roboshop/component/mongodb.sh
@@ -42,14 +42,14 @@ stat $?
 
 echo -n "Extracting the ${COMPONENT} schema :"
 cd /tmp
-unzip ${COMPONENT}.zip      &>> ${LOGFILE}
+unzip ${COMPONENT}.zip   &>> ${LOGFILE}
 stat $?
 
-echo -n "Injecting the ${COMPONENT} schema :"
-cd ${COMPONENT}-main
-mongo < catalogue.js   &>> ${LOGFILE}
-mongo < users.js       &>> ${LOGFILE}
-stat $?
+#echo -n "Injecting the ${COMPONENT} schema :"
+#cd ${COMPONENT}-main
+#mongo < catalogue.js   &>> ${LOGFILE}
+#mongo < users.js       &>> ${LOGFILE}
+#stat $?
 
 
 

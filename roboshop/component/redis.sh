aef542ca22c69d3cd687176ba18e499a72fb8635
sarvan <msaravanakumar312@gmail.com>
2023-08-08 19:22:03 +0530

    Auto push

--- roboshop/component/redis.sh
+++ roboshop/component/redis.sh
@@ -28,6 +28,11 @@ echo -n "Installing the ${COMPONENT} redis :"
 yum install redis-6.2.12 -y  &>> ${LOGFILE}
 stat $?
 
+echo -n "Enableing the ${COMPONENT} redis :"
+sed -ie 's/127.0.0.0/0.0.0.0/g' /etc/${COMPONENT}.conf  &>> ${LOGFILE}
+sed -ie 's/127.0.0.0/0.0.0.0/g' /etc/${COMPONENT}/${COMPONENT}.conf  &>> ${LOGFILE}
+stat $?
+
 
 
 

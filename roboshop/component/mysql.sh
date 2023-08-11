ae79085bf0de95cf1e7898a82fee9344b2c13831
sarvan <msaravanakumar312@gmail.com>
2023-08-11 20:50:14 +0530

    Auto push

--- roboshop/component/mysql.sh
+++ roboshop/component/mysql.sh
@@ -23,23 +23,10 @@ echo -n "Extracting the default mysql root password :"
 DEFAULT_ROOT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk -F " " '{print $NF}')
 stat $?
 
-echo "show databases;" | mysql -uroot -pRoboshop@1  &>> ${LOGFILE}
-if [ $? -ne 0 ]; then
-    echo -n "Performing default password reset of root account:"
-    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1'" | mysql --connect-expired-password -uroot -p$DEFAULT_ROOT_PASSWORD  &>>  ${LOGFILE}
-    stat $?
-fi
-
-echo "show plugins;" | mysql -uroot -pRoboshop@1 | grep validate_password &>>  ${LOGFILE}
-if [ $? -ne 0 ]; then 
-    echo -n "Uninstalling password_validate plugin:"
-    echo "uninstall plugin validate_password" | mysql -uroot -pRoboshop@1 &>> ${LOGFILE}
-    stat $?
-fi
-
-
 #This sholud happen only once and that too for the first time,when it runs for the second time,jobs failure.
 #We need ensure that runs only one.
+
+
 
 
 

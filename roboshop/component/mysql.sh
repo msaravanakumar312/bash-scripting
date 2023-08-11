d5d9c8c9d0f11e5eac37539bb75dd5039787aeec
sarvan <msaravanakumar312@gmail.com>
2023-08-11 15:06:21 +0530

    Auto push

--- roboshop/component/mysql.sh
+++ roboshop/component/mysql.sh
@@ -1,26 +1,10 @@
 #!/bin/bash
 
-set -e
-
-USER_ID=$(id -u)
 COMPONENT=mysql
-LOGFILE="/tmp/${COMPONENT}.log "
 
+source=component/common.sh
 
-if [ $USER_ID -ne 0 ] ; then
-    echo -e "\e[32m script is executed by the root user or with a sudo previllages \e[0m \n \t Example : sudo bash wrapper.sh frontend"
-    exit 1
-fi
-
-stat () {
-if [ $1 -eq 0 ] ; then
-    echo -e "\e[32m Success \e[0m"
-else
-    echo -e "\e[31 Failure \e[0m"
-fi
-}
-
-echo -e "\e[35m Configuring ${COMPONENT} \e[0m"
+echo -e "\e[35m Configuring ${COMPONENT}.....! \e[0m"
 
 echo -n "Configuring the ${COMPONENT} repo :"
 # curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo

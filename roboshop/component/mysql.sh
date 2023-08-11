77f43b0acf71151d81bbedd68f81b7351d3ddd38
sarvan <msaravanakumar312@gmail.com>
2023-08-11 22:34:41 +0530

    Auto push

--- roboshop/component/mysql.sh
+++ roboshop/component/mysql.sh
@@ -39,17 +39,17 @@ stat $?
 fi
 
 echo -n "Downloading the ${COMPONENT} schema :"
-curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
+curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
 stat $?
 
 echo -n "Extracting the ${COMPONENT} schema :"
-cd /tmp/
+cd /tmp
 unzip -o /tmp/${COMPONENT}.zip  &>> ${LOGFILE}
 stat $?
 
 echo -n "Injecting the schema :" 
 cd ${COMPONENT}-main    &>> ${LOGFILE}
-mysql -uroot -pRoboShop@1 <shipping.sql &>> ${LOGFILE}
+mysql -u root -pRoboShop@1 <shipping.sql &>> ${LOGFILE}
 stat $?
 
 echo -e "\n \e[32m ${COMPONENT} Installation Is Completed \e[0m \n"

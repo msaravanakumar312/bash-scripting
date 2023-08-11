96a7298a9385bec6a0cfc0a23da50dbd92db8819
sarvan <msaravanakumar312@gmail.com>
2023-08-11 16:39:18 +0530

    Auto push

--- roboshop/component/mysql.sh
+++ roboshop/component/mysql.sh
@@ -26,13 +26,13 @@ stat $?
 echo "show databases;" | mysql -uroot -pRoboshop@1  &>> ${LOGFILE}
 if [ $? -ne 0 ]; then
     echo -n "Performing default password reset of root account:"
-    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1'" | mysql --connect-expired-password -uroot -p$DEFAULT_ROOT_PASSWORD  &>> ${LOGFILE}
+    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1'" | mysql  --connect-expired-password -uroot -p$DEFAULT_ROOT_PASSWORD  &>>  ${LOGFILE}
     stat $?
 fi
 
-echo "show plugins;" | mysql -uroot -pRoboshop@1 | grep validate_password &>> ${LOGFILE}
+echo "show plugins;" | mysql -uroot -pRoboshop@1 | grep validate_password &>>  ${LOGFILE}
 if [ $? -ne 0 ]; then 
-    echo -n "Uninstalling Password-validate plugin:"
+    echo -n "Uninstalling password-validate plugin:"
     echo "uninstall plugin validate_password" | mysql -uroot -pRoboshop@1 &>> ${LOGFILE}
     stat $?
 fi

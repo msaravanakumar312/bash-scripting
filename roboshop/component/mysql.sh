2081f6623fced475408eb108e9e9907646ccf2be
sarvan <msaravanakumar312@gmail.com>
2023-08-11 22:58:17 +0530

    Auto push

--- roboshop/component/mysql.sh
+++ roboshop/component/mysql.sh
@@ -33,24 +33,24 @@ if [ $? -ne 0 ]; then
 fi
 echo "show plugins;" | mysql -uroot -pRoboshop@1 | grep validate_password  &>> ${LOGFILE}
 if [ $? -ne 0 ]; then 
-echo -n "Uninstalling Password-validate plugin :"
-echo "uninstall plugin validate_password" | mysql -uroot -pRoboshop@1  &>> ${LOGFILE}
-stat $?
+    echo -n "Uninstalling Password-validate plugin :"
+    echo "uninstall plugin validate_password" | mysql -uroot -pRoboshop@1  &>> ${LOGFILE}
+    stat $?
 fi
 
 echo -n "Downloading the ${COMPONENT} schema :"
-curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
-stat $?
+    curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+    stat $?
 
 echo -n "Extracting the ${COMPONENT} schema :"
-cd /tmp
-unzip -o ${COMPONENT}.zip  &>> ${LOGFILE}
-stat $?
+    cd /tmp
+    unzip -o ${COMPONENT}.zip  &>> ${LOGFILE}
+    stat $?
 
 echo -n "Injecting the schema :" 
-cd ${COMPONENT}-main    
-mysql -u root -pRoboShop@1 <shipping.sql &>> ${LOGFILE}
-stat $?
+    cd ${COMPONENT}-main    
+    mysql -u root -pRoboShop@1 <shipping.sql    &>> ${LOGFILE}
+    stat $?
 
 echo -e "\n \e[32m ${COMPONENT} Installation Is Completed \e[0m \n"
 

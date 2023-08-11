d675500ba8011e297caae2773b533bd27470433d
sarvan <msaravanakumar312@gmail.com>
2023-08-11 15:07:47 +0530

    Auto push

--- roboshop/component/mysql.sh
+++ roboshop/component/mysql.sh
@@ -7,7 +7,7 @@ source=component/common.sh
 echo -e "\e[35m Configuring ${COMPONENT}.....! \e[0m"
 
 echo -n "Configuring the ${COMPONENT} repo :"
-# curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo  &>> ${LOGFILE}
 stat $?
 
 echo -n "Installing ${COMPONENT} :"

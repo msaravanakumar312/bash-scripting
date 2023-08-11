23d6385c5374f3438e715aa8e816c15a5df4ebec
sarvan <msaravanakumar312@gmail.com>
2023-08-11 16:35:34 +0530

    Auto push

--- roboshop/component/mysql.sh
+++ roboshop/component/mysql.sh
@@ -19,22 +19,22 @@ systemctl enable mysqld     &>> ${LOGFILE}
 systemctl start mysqld      &>> ${LOGFILE}
 stat $?
 
-echo -n "Extracting the mysql default root password :"
+echo -n "Extracting the default mysql root password :"
 DEFAULT_ROOT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk -F " " '{print $NF}')
 stat $?
 
 echo "show databases;" | mysql -uroot -pRoboshop@1  &>> ${LOGFILE}
 if [ $? -ne 0 ]; then
     echo -n "Performing default password reset of root account:"
-    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1'" | mysql  --connect-expired-password -uroot -p$DEFAULT_ROOT_PASSWORD  &>> ${LOGFILE}
+    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1'" | mysql --connect-expired-password -uroot -p$DEFAULT_ROOT_PASSWORD  &>> ${LOGFILE}
     stat $?
 fi
 
 echo "show plugins;" | mysql -uroot -pRoboshop@1 | grep validate_password &>> ${LOGFILE}
 if [ $? -ne 0 ]; then 
-echo -n "Uninstalling password-validate plugin :"
-echo "uninstall plugin validate_password" | mysql -uroot -pRoboshop@1 &>> ${LOGFILE}
-stat $?
+    echo -n "Uninstalling Password-validate plugin:"
+    echo "uninstall plugin validate_password" | mysql -uroot -pRoboshop@1 &>> ${LOGFILE}
+    stat $?
 fi
 
 

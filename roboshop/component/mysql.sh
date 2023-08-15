87eb2c6433672722b87e197f73145f25a17f54d5
sarvan <msaravanakumar312@gmail.com>
2023-08-15 08:01:28 +0530

    Auto push

--- roboshop/component/mysql.sh
+++ roboshop/component/mysql.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+USER_ID=$(id -u)
 COMPONENT=mysql
+LOGFILE="/tmp/${COMPONENT}.log "
 
-source component/common.sh
 
     echo -e "\e[35m Configuring ${COMPONENT}.....! \e[0m"
 
@@ -32,6 +33,29 @@ if [ $? -ne 0 ]; then
     stat $?
 fi
     
+    echo "show plugins;" | mysql -uroot -pRoboshop@1 | grep validate_password  &>> ${LOGFILE}
+if [ $? -ne 0 ]; then 
+    echo -n "Uninstalling Password-validate plugin :"
+    echo "uninstall plugin validate_password" | mysql -uroot -pRoboshop@1  &>> ${LOGFILE}
+    stat $?
+fi
+
+    echo -n "Downloading the ${COMPONENT} schema :"
+    curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+    stat $?
+
+    echo -n "Extracting the ${COMPONENT} schema :"
+    cd /tmp
+    unzip -o /tmp/${COMPONENT}.zip  &>> ${LOGFILE}
+    stat $?
+
+    echo -n "Injecting the schema :" 
+    cd ${COMPONENT}-main    
+    mysql -u root -pRoboshop@1 <shipping.sql    &>> ${LOGFILE}
+    stat $?
+
+    echo -e "\n \e[32m ${COMPONENT} Installation Is Completed \e[0m \n"
+
 
 
 

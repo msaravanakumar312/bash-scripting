f3c5adb274ed36c53e9ff1bc26925136d0c8e429
sarvan <msaravanakumar312@gmail.com>
2023-08-15 06:40:32 +0530

    Auto push

--- roboshop/component/frontend.sh
+++ roboshop/component/frontend.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
+USER_ID=$(id -u)
 COMPONENT=frontend
-
-#This is how we imporrt funtions that are declared in a defferent file using soure
-source component/common.sh
-#NODEJS       #calling nodejs funtions
+LOGFILE="/tmp/${COMPONENT}.log "
 
 
-echo -e "\n \e[32m ${COMPONENT} Installation Is Completed \e[0m \n"
+if [ $USER_ID -ne 0 ] ; then
+    echo -e "\e[32m script is executed by the root user or with a sudo previllages \e[0m \n \t Example : sudo bash wrapper.sh frontend"
+    exit 1
+fi
+
+stat () {
+if [ $1 -eq 0 ] ; then
+    echo -e "\e[32m Success \e[0m"
+else
+    echo -e "\e[31 Failure \e[0m"
+fi
+}
+
+echo -e "\e[35m Configuring ${COMPONENT} \e[0m
+echo -n "Installing frontend :"
+yum install nginx -y   &>> ${LOGFILE}
+stat $?

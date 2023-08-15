eb7d5b414230e1cdc5fb4b9011a484a891f55d09
sarvan <msaravanakumar312@gmail.com>
2023-08-15 07:32:30 +0530

    Auto push

--- roboshop/component/mysql.sh
+++ roboshop/component/mysql.sh
@@ -31,29 +31,7 @@ if [ $? -ne 0 ]; then
     echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'Roboshop@1'" | mysql --connect-expired-password -uroot -p$DEFAULT_ROOT_PASSWORD &>> ${LOGFILE}
     stat $?
 fi
-    echo "show plugins;" | mysql -uroot -pRoboshop@1 | grep validate_password  &>> ${LOGFILE}
-if [ $? -ne 0 ]; then 
-    echo -n "Uninstalling Password-validate plugin :"
-    echo "uninstall plugin validate_password" | mysql -uroot -pRoboshop@1  &>> ${LOGFILE}
-    stat $?
-fi
-
-    echo -n "Downloading the ${COMPONENT} schema :"
-    curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
-    stat $?
-
-    echo -n "Extracting the ${COMPONENT} schema :"
-    cd /tmp
-    unzip -o /tmp/${COMPONENT}.zip  &>> ${LOGFILE}
-    stat $?
-
-    echo -n "Injecting the schema :" 
-    cd ${COMPONENT}-main    
-    mysql -u root -pRoboshop@1 <shipping.sql    &>> ${LOGFILE}
-    stat $?
-
-    echo -e "\n \e[32m ${COMPONENT} Installation Is Completed \e[0m \n"
-
+    
 
 
 
--- roboshop/component/shipping.sh
+++ roboshop/component/shipping.sh
@@ -1,52 +1,24 @@
-#!/bin/bash
-
-USER_ID=$(id -u)
-COMPONENT=frontend
-LOGFILE="/tmp/${COMPONENT}.log "
-
-
-if [ $USER_ID -ne 0 ] ; then
-    echo -e "\e[32m script is executed by the root user or with a sudo previllages \e[0m \n \t Example : sudo bash wrapper.sh frontend"
-    exit 1
+echo "show plugins;" | mysql -uroot -pRoboshop@1 | grep validate_password  &>> ${LOGFILE}
+if [ $? -ne 0 ]; then 
+    echo -n "Uninstalling Password-validate plugin :"
+    echo "uninstall plugin validate_password" | mysql -uroot -pRoboshop@1  &>> ${LOGFILE}
+    stat $?
 fi
 
-stat () {
-if [ $1 -eq 0 ] ; then
-    echo -e "\e[32m Success \e[0m"
-else
-    echo -e "\e[31 Failure \e[0m"
-fi
-}
+    echo -n "Downloading the ${COMPONENT} schema :"
+    curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+    stat $?
 
-echo -e "\e[35m Configuring ${COMPONENT} \e[0m
-echo -n "Installing frontend :"
-yum install nginx -y   &>> LOGFILE
-stat $?
+    echo -n "Extracting the ${COMPONENT} schema :"
+    cd /tmp
+    unzip -o /tmp/${COMPONENT}.zip  &>> ${LOGFILE}
+    stat $?
 
-echo -n "starting nginx :"
-systemctl enable nginx    &>> LOGFILE
-systemctl start nginx     &>> LOGFILE
-stat $?
+    echo -n "Injecting the schema :" 
+    cd ${COMPONENT}-main    
+    mysql -u root -pRoboshop@1 <shipping.sql    &>> ${LOGFILE}
+    stat $?
+
+    echo -e "\n \e[32m ${COMPONENT} Installation Is Completed \e[0m \n"
 
 
-echo -n "Downloading the ${COMPONENT} component :"
-curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
-stat $?
-
-echo -n "Clean up the frontend :"
-cd /usr/share/nginx/html
-rm -rf *      &>> LOGFILE
-stat $?
-
-echo -n "Extracting the ${COMPONENT} :"
-unzip /tmp/${COMPONENT}.zip    &>> LOGFILE
-mv ${COMPONENT}-main/* .
-mv static/* .
-rm -rf ${COMPONENT}-main README.md     &>> LOGFILE 
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
-stat $?
-
-echo -n "Restarting ${COMPONENT} :"
-systemctl daemon-reload      &>> LOGFILE  
-systemctl restart nginx      &>> LOGFILE
-stat $?

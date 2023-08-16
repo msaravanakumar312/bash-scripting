0c99d39a97ca4d530c7f0a62931fa4d01d3b0259
sarvan <msaravanakumar312@gmail.com>
2023-08-16 22:07:34 +0530

    Auto push

--- roboshop/component/mysql.sh
+++ roboshop/component/mysql.sh
@@ -38,3 +38,12 @@ if [ $? -ne 0 ]; then
     echo "uninstall plugin validate_password" | mysql -uroot -pRoboshop@1  &>> ${LOGFILE}
     stat $?
 fi
+
+echo -n "Downloading the ${COMPONENT} schema :"
+    curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+    stat $?
+
+    echo -n "Extracting the ${COMPONENT} schema :"
+    cd /tmp
+    unzip -o ${COMPONENT}.zip  &>> ${LOGFILE}
+    stat $?

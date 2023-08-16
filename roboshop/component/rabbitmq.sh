ed3b5f693239c1cc96d422506e50355292584ab6
sarvan <msaravanakumar312@gmail.com>
2023-08-16 23:12:43 +0530

    Auto push

--- roboshop/component/rabbitmq.sh
+++ roboshop/component/rabbitmq.sh
@@ -1,27 +1,38 @@
 
-    
-    echo "show plugins;" | mysql -uroot -pRoboshop@1 | grep validate_password  &>> ${LOGFILE}
-if [ $? -ne 0 ]; then 
-    echo -n "Uninstalling Password-validate plugin :"
-    echo "uninstall plugin validate_password" | mysql -uroot -pRoboshop@1  &>> ${LOGFILE}
-    stat $?
-fi
+   #!/bin/bash
 
-    echo -n "Downloading the ${COMPONENT} schema :"
-    curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
-    stat $?
-
-    echo -n "Extracting the ${COMPONENT} schema :"
-    cd /tmp
-    unzip -o ${COMPONENT}.zip  &>> ${LOGFILE}
-    stat $?
-
-    echo -n "Injecting the schema :" 
-    cd ${COMPONENT}-main    
-    mysql -u root -pRoboshop@1 <shipping.sql    &>> ${LOGFILE}
-    stat $?
-
-    echo -e "\n \e[32m ${COMPONENT} Installation Is Completed \e[0m \n"
+USER_ID=$(id -u)
+COMPONENT=rabbitmq
+LOGFILE="/tmp/${COMPONENT}.log "
 
 
+source component/common.sh
 
+ 
+echo -e "\e[35m Configuring ${COMPONENT} \e[0m"
+
+echo -n "Configuring the ${COMPONENT} repositories :"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>> {LOGFILE}
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>> {LOGFILE}
+stat $?
+
+
+echo -n "Installing the ${COMPONENT} :"
+yum install rabbitmq-server -y   &>> {LOGFILE}
+stat $?
+
+echo -n "starting nginx :"
+systemctl enable ${COMPONENT}-server  &>> {LOGFILE}
+systemctl start ${COMPONENT}-server   &>> {LOGFILE}
+stat $?
+
+echo -n "Creating ${COMPONENT} user account :"
+rabbitmqctl add_user roboshop roboshop123  &>> {LOGFILE}
+stat $?
+
+echo -n "Configuring ${COMPONENT} permissions :"
+rabbitmqctl set_user_tags roboshop administrator         &>> {LOGFILE}
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>> {LOGFILE} 
+stat $?
+
+echo -e "\n \e[32m ${COMPONENT} Installation Is Completed \e[0m \n"

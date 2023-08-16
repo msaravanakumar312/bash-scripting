1add2541f2c46aff15e7aee71db6d55bd54f373c
sarvan <msaravanakumar312@gmail.com>
2023-08-16 23:52:07 +0530

    Auto push

--- roboshop/component/common.sh
+++ roboshop/component/common.sh
@@ -124,8 +124,8 @@ PYTHON() {
     pip3 install -r requirements.txt &>> ${LOGFILE}
     stat $?
 
-    USER_ID=$(id -u roboshop)
-    GROUP_ID=$(id -g roboshop)
+    USERID=$(id -u roboshop)
+    GROUPID=$(id -g roboshop)
 
     echo -n "Updating the uid and gid in the ${COMPONENT}.ini file :"
     sed -i -e "/^uid/ c uid=${USERID}" -e "/^gid c gid=${GROUPID}" /home/${APPUSER}/${COMPONENT}/${COMPONENT}.ini 

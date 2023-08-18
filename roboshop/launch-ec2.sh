a45a268bc6166e12dbdd1fff8d133c687513d04f
sarvan <msaravanakumar312@gmail.com>
2023-08-18 12:35:42 +0530

    Auto push

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -14,7 +14,7 @@ SG_ID="sg-014f2d8e59de1c638"            #b55-allow-all  security group id
 
 aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"
 
-aws ec2 run-instances --image-id ami-0c1d144c8fdd8d690 --instance-type t3.micro --security-group-ids sg-014f2d8e59de1c638 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"
+aws ec2 run-instances --image-id ami-0c1d144c8fdd8d690 --instance-type t3.micro --security-group-ids sg-014f2d8e59de1c638 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=lab}]"
 
  
 

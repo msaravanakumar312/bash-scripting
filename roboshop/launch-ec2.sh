67e68197b50893c9cc425e3fbf08738cb3441739
sarvan <msaravanakumar312@gmail.com>
2023-08-19 00:03:48 +0530

    Auto push

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -3,8 +3,9 @@
 COMPONENT=$1
 INSTANCE_TYPE="t3.micro"
 HOSTEDZONEID="Z0475481NBO60TX4OZ6V"
+ENV=$2
 
-if [ -z $1 ] ; then
+if [ -z $1 ] || [ -z $2 ] ; then
     echo -e "\e[31m COMPONENT NAME IS NEEDED \e[0m \n \t \t"
     echo -e "\e[35m Ex Usage \e[0m |$ bash launch-ec2.sh shipping"
     exit 1
@@ -16,18 +17,18 @@ SG_ID="$(aws ec2 describe-security-groups --filters Name=group-name,Values=b55-a
 
 
 Create_ec2 () {
-echo -e "*******Creating \e[35m ${COMPONENT} \e[0m server is in progress******"
+echo -e "*******Creating \e[35m ${COMPONENT}-${ENV} \e[0m server is in progress******"
 PRIVATEIP="$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')"
 
-echo -e "Private ip of the $COMPONENT is $PRIVATEIP \n\n" 
-echo -e "Creating Dns record of ${COMPONENT} :"
+echo -e "Private ip of the $COMPONENT-${ENV} is $PRIVATEIP \n\n" 
+echo -e "Creating Dns record of ${COMPONENT}-${ENV} :"
 
-sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATEIP}/" route53.json > /tmp/r53.json
+sed -e "s/COMPONENT/${COMPONENT}-${ENV}/" -e "s/IPADDRESS/${PRIVATEIP}/" route53.json > /tmp/r53.json
 
 cat /tmp/r53.json
 
 aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/r53.json
-echo -e "\e[35m *** Creating Dns Record for the $COMPONENT has Completed *** \e[0m"
+echo -e "\e[35m *** Creating Dns Record for the $COMPONENT-${ENV} has Completed *** \e[0m"
 }
 
 if [ $1 == "all" ]; then

bc60dd41fc55792e061ce3a8b93d8f87c2ed2d88
sarvan <msaravanakumar312@gmail.com>
2023-08-18 20:39:54 +0530

    Auto push

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -14,6 +14,8 @@ AMI_ID="$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-Ce
 SG_ID="$(aws ec2 describe-security-groups --filters Name=group-name,Values=b55-allow-all | jq '.SecurityGroups[].GroupName' | sed -e 's/"//g')"    #b55-allow-all  security group id
 
 
+
+Create_ec2 () {
 echo -e "*******Creating \e[35m ${COMPONENT} \e[0m server is in progress******"
 PRIVATEIP="$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')"
 
@@ -26,3 +28,14 @@ cat /tmp/r53.json
 
 aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/r53.json
 echo -e "\e[35m *** Creating Dns Record for the $COMPONENT has Completed *** \e[0m"
+}
+
+if [ $1 == "all" ]; then
+    echo component in frontend mongodb catalogue user redis shipping payment cart mysql rabbitmq; do
+    COMPONENT=$component 
+    Create_ec2
+done
+
+else 
+    Create_ec2
+fi

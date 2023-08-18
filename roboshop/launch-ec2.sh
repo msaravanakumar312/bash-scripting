0a0a1e56e3dae561e143f90d18d71e6af532f169
sarvan <msaravanakumar312@gmail.com>
2023-08-18 19:57:52 +0530

    Auto push

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -10,13 +10,16 @@ fi
 AMI_ID="$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq ".Images[].ImageId" | sed -e 's/"//g')"
 SG_ID="$(aws ec2 describe-security-groups --filters Name=group-name,Values=b55-allow-all | jq '.SecurityGroups[].GroupName' | sed -e 's/"//g')"    #b55-allow-all  security group id
 INSTANCE_TYPE="t3.micro"
-
-
+HOSTEDZONEID="Z0475481NBO60TX4OZ6V"
 
 echo -e "*******Creating \e[35m ${COMPONENT} \e[0m server is in progress******"
-PRIVATEIP="$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=lab}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')"
+PRIVATEIP="$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')"
 
-echo "Private ip of the $COMPONENT is $PRIVATEIP"
+echo -e "Private ip of the $COMPONENT is $PRIVATEIP /n/n" 
+echo -e "Creating Dns record of ${COMPONENT} :"
+
+sed -e 's/COMPONENT/${COMPONENT}/' -e 's/IPADDRESS/${IPADDRESS}/' route53.json > /tmp/r53.json
+aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/r53.json
 
 
 

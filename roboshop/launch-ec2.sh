7633a14edd56f6ba71dd0a25e5fa3a55e8533af1
sarvan <msaravanakumar312@gmail.com>
2023-08-28 17:41:44 +0530

    Auto push

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -20,16 +20,16 @@ Create_ec2 () {
 echo -e "*******Creating \e[35m ${COMPONENT} \e[0m server is in progress******"
 PRIVATEIP="$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}-${ENV}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')"
 
-echo -e "Private ip of the $COMPONENT is $PRIVATEIP \n\n" 
-echo -e "Creating DNS record of ${COMPONENT} :"
+echo -e "Private Ip Address of the $COMPONENT is $PRIVATEIP \n\n" 
+echo -e "Creating DNS record of ${COMPONENT}: "
 
 sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATEIP}/" route53.json > /tmp/r53.json
 
 aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/r53.json
-echo -e "\e[35m *** Creating Dns Record for the $COMPONENT has Completed *** \e[0m"
+echo -e "\e[35m *** Creating Dns Record for the $COMPONENT has Completed *** \e[0m \n\n"
 }
 
-if [ $1 == "all" ]; then
+if [ "$1" == "all" ]; then
     for component in frontend mongodb catalogue user redis shipping payment cart mysql rabbitmq; do
     COMPONENT=$component 
     Create_ec2

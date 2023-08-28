9ea011abac1659ee00fdf5ac214f62a0d4c6d187
sarvan <msaravanakumar312@gmail.com>
2023-08-28 17:27:27 +0530

    Auto push

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -20,12 +20,10 @@ Create_ec2 () {
 echo -e "*******Creating \e[35m ${COMPONENT} \e[0m server is in progress******"
 PRIVATEIP="$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}-${ENV}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')"
 
-echo -e "Private ip of the $COMPONENT-${ENV} is $PRIVATEIP \n\n" 
-echo -e "Creating Dns record of ${COMPONENT} :"
+echo -e "Private ip of the $COMPONENT is $PRIVATEIP \n\n" 
+echo -e "Creating DNS record of ${COMPONENT} :"
 
-sed -e "s/COMPONENT/${COMPONENT}-${ENV}/" -e "s/IPADDRESS/${PRIVATEIP}/" route53.json > /tmp/r53.json
-
-cat /tmp/r53.json
+sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATEIP}/" route53.json > /tmp/r53.json
 
 aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/r53.json
 echo -e "\e[35m *** Creating Dns Record for the $COMPONENT has Completed *** \e[0m"

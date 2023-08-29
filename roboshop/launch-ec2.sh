e8b40f1dc5e59447e8c45223843e567e1b4b0b2a
sarvan <msaravanakumar312@gmail.com>
2023-08-29 09:13:11 +0530

    Auto push

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -16,7 +16,7 @@ AMI_ID="$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-Ce
 SG_ID="$(aws ec2 describe-security-groups --filters Name=group-name,Values=b55-allow-all | jq '.SecurityGroups[].GroupName' | sed -e 's/"//g')"    #b55-allow-all  security group id
 
 Create_ec2 () {
-    echo -e "*******Creating \e[35m ${COMPONENT} \e[0m server is in progress******"
+    echo -e "******* Creating \e[35m ${COMPONENT} \e[0m server is in progress ******"
     PRIVATEIP="$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')"
 
     echo -e "Private Ip Address of the $COMPONENT is $PRIVATEIP \n\n" 

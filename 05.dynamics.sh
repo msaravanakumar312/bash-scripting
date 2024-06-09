a523eaf449027ea1bf1e5e1eae67001c2fb7fafa
sarvan <msaravanakumar312@gmail.com>
2024-06-09 13:58:23 +0530

    Updated dynamic file

--- 05.dynamics.sh
+++ 05.dynamics.sh
@@ -4,5 +4,5 @@ DATE="$(date +%F)"
 SESSION_COUNT=$(who | wc -l)
 
 echo -e "Today date is \e[32m $DATE \e[0m"
-echo -e "Today month is \e[32m $MONTH \e[0m"
+echo -e "Today time is \e[32m $TIME \e[0m"
 echo -e "Total number of active session count is \e[32m $SESSION_COUNT \e[0m"

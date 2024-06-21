e8415d69fb8123804f04612fda933dc18f8fd0be
sarvan <msaravanakumar312@gmail.com>
2024-06-21 11:53:01 +0530

    Updated dynamic variables

--- 05.dynamics.sh
+++ 05.dynamics.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-DATE="$(date +%F)"
-SESSION_COUNT=$(who | wc -l)
+#DATE="$(date +%F)"
+#SESSION_COUNT=$(who | wc -l)
 
-echo -e "Today date is \e[32m $DATE \e[0m"
-echo -e "Today time is \e[32m $TIME \e[0m"
-echo -e "Total number of active session count is \e[32m $SESSION_COUNT \e[0m"
+#echo -e "Today date is \e[32m $DATE \e[0m"
+#echo -e "Today time is \e[32m $TIME \e[0m"
+#echo -e "Total number of active session count is \e[32m $SESSION_COUNT \e[0m"
+
+
+DATE="$(date +%F)"
+SESSION_COUNT="(who | wc-l)"
+
+echo  -e "Today date is \e[32m $DATE \e[0m"
+echo -e "The total number of session is \e [33m $SESSION_COUNT \e[0m"
+

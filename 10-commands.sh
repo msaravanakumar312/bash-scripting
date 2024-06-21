675800e33cd52512563bda62226cb367ee31656e
sarvan <msaravanakumar312@gmail.com>
2024-06-21 15:53:17 +0530

    Updated commands

--- 10-commands.sh
+++ 10-commands.sh
@@ -23,7 +23,7 @@ hai() {
 Stat() {
     echo "Number of session opened are $(who | wc -l)"
     echo "Today date is $(date +%F)"
-    echo "Average cpu utilizations in last 5 minutes $(uptime | awk -F : '{print $NF})' | awk -F , '{print $2}')"
+    echo "Average cpu utilizations in last 5 minutes $(uptime | awk -F : '{print $NF})' | awk -F : '{print $2}')"
 
     hai
 }

78a1ea42738bb97d1e6d51dd6c5149c3a6cdae4f
sarvan <msaravanakumar312@gmail.com>
2024-07-06 13:19:37 +0530

    Updated conmmands

--- 13-functions.sh
+++ 13-functions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-f() {
+stat() {
     echo "Today date is $(date +%F)"
     echo "Average CPU utilization is $(uptime | awk -F : '{print $NF}', awk -F : '{print $2}')"
     echo "Number of logged in session is $(who | wc-l)"
 }
-fi
+stat

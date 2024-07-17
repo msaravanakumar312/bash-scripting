80c88fe0ba423a025f3042993d49d5ada4c9682d
sarvan <msaravanakumar312@gmail.com>
2024-07-17 17:39:17 +0530

    Updated comments

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
 
+USER_ID=$(id -u)
 
-echo "I am frontend"
+if [ USER_ID -ne 0] ; then
+    echo -e "\e[32m Script is expected to excuted by the root user or sudo previllage \e[0m \n \t Example: sudo bash wrapper.sh"
+    exit 1
+fi
+
+echo -e "\e[31m configuring frontend...! \e[0m \n"
+
+echo -e "Installing frontend :"
+yum install nginx -y   &>> /tmp/frontend.log
+
+if [$? -eq 0] ; then
+    echo -e "\e[31m Success \e[0m"
+else
+    echo -e "\e[31m Failure \e[0m"
+
+# systemctl enable nginx
+# systemctl start nginx
+# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+# cd /usr/share/nginx/html
+# rm -rf *
+# unzip /tmp/frontend.zip
+# mv frontend-main/* .
+# mv static/* .
+# rm -rf frontend-main README.md
+# mv localhost.conf /etc/nginx/default.d/roboshop.conf

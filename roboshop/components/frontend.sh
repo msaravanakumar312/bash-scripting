75a0b7b4e13f02805e0f179143a2299f9f3cf68f
sarvan <msaravanakumar312@gmail.com>
2024-07-17 21:35:02 +0530

    Updated comments

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -12,7 +12,7 @@ if [ $USER_ID -ne 0 ] ; then
 fi
 
 stat() {
-    if [ $? -eq 0 ] ; then
+    if [ $1 -eq 0 ] ; then
         echo -e "\e[31m Success \e[0m"
     else
         echo -e "\e[31m Failure \e[0m"

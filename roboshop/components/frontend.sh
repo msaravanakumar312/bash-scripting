08328b5581a0659aef61e713f5393b266ab2a430
sarvan <msaravanakumar312@gmail.com>
2024-07-17 22:08:39 +0530

    Updated comments

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -29,7 +29,7 @@ systemctl start nginx   &>> ${LOGFILE}
 stat $?
 
 echo -n "Downloading the ${COMPONENT} component :"
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/{COMPONENT}/archive/main.zip"
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
 stat $?
 
 

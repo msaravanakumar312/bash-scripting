cb27550ec3969e46b26d49bf1608242db8228b7e
sarvan <msaravanakumar312@gmail.com>
2024-07-17 18:01:12 +0530

    Updated comments

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -7,39 +7,40 @@ if [ $USER_ID -ne 0 ] ; then
     exit 1
 fi
 
+stat() {
+    if [ $? -eq 0 ] ; then
+        echo -e "\e[31m Success \e[0m"
+    else
+        echo -e "\e[31m Failure \e[0m"
+    fi
+}
+
 echo -e "\e[31m configuring frontend...! \e[0m \n"
 
 echo -n "Installing frontend :"
 yum install nginx -y   &>> /tmp/frontend.log
-
-if [ $? -eq 0 ] ; then
-    echo -e "\e[31m Success \e[0m"
-else
-    echo -e "\e[31m Failure \e[0m"
-fi
+stat $?
 
 echo -n "Starting Nginx:"
 systemctl enable nginx  &>> /tmp/frontend.log
 systemctl start nginx   &>> /tmp/frontend.log
-
-if [ $? -eq 0 ] ; then
-    echo -e "\e[31m Success \e[0m"
-else
-    echo -e "\e[31m Failure \e[0m"
-fi
+stat $?
 
 echo -n "Downloading the frontend component :"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+stat $?
 
-if [ $? -eq 0 ] ; then
-    echo -e "\e[31m Success \e[0m"
-else
-    echo -e "\e[31m Failure \e[0m"
-fi
 
-# cd /usr/share/nginx/html
-# rm -rf *
-# unzip /tmp/frontend.zip
+echo -n "Clean up of frontend:"
+cd /usr/share/nginx/html
+rm -rf *   &>> /tmp/frontend.log
+stat $?
+
+echo -n "Extracing frontend:"
+unzip /tmp/frontend.zip   &>> /tmp/frontend.log
+stat $?
+
+
 # mv frontend-main/* .
 # mv static/* .
 # rm -rf frontend-main README.md

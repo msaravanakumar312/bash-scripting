16841b6f618c71168bf4b4e679b25e4f4cffa837
sarvan <msaravanakumar312@gmail.com>
2024-07-17 17:44:10 +0530

    Updated comments

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -12,7 +12,7 @@ echo -e "\e[31m configuring frontend...! \e[0m \n"
 echo -e "Installing frontend :"
 yum install nginx -y   &>> /tmp/frontend.log
 
-if [$? -eq 0] ; then
+if [ $? -eq 0 ] ; then
     echo -e "\e[31m Success \e[0m"
 else
     echo -e "\e[31m Failure \e[0m"

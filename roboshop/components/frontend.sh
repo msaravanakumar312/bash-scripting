ee0306ad40d2bb99fee27054b25783629c5b2acc
sarvan <msaravanakumar312@gmail.com>
2024-07-17 17:54:29 +0530

    Updated comments

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -9,7 +9,7 @@ fi
 
 echo -e "\e[31m configuring frontend...! \e[0m \n"
 
-echo -e "Installing frontend :"
+echo -n "Installing frontend :"
 yum install nginx -y   &>> /tmp/frontend.log
 
 if [ $? -eq 0 ] ; then

49c15bbbd2953af117340cff2038de1a454ac6f9
sarvan <msaravanakumar312@gmail.com>
2024-07-18 13:09:13 +0530

    Updated comments

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -43,13 +43,13 @@ stat $?
 echo -n "Copying the ${COMPONENT} to ${APPUSER} home directory :"
 cd /home/${APPUSER}/
 rm -rf ${COMPONENT}    &>> ${LOGFILE}
-unzip /tmp/${COMPONENT}.zip   &>> ${LOGFILE}
+unzip -o /tmp/${COMPONENT}.zip   &>> ${LOGFILE}
 mv ${COMPONENT}-main ${COMPONENT}
 stat $?
 
-#echo -n "Changing the ownership :"
-#chown -R ${APPUSER}:${APPUSER}  /home/${APPUSER}/${COMPONENT}
-#stat $?
+echo -n "Changing the ownership :"
+chown -R ${APPUSER}:${APPUSER}  /home/${APPUSER}/${COMPONENT}
+stat $?
 
 #echo -n "Generating the ${COMPONENT} artifacts :"
 #cd /home/${APPUSER}/${COMPONENT}

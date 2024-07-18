27a465f4b78e3011f6aecdf3ed4e167dd64399a0
sarvan <msaravanakumar312@gmail.com>
2024-07-18 13:55:08 +0530

    Updated comments

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -51,7 +51,7 @@ chown -R ${APPUSER}:${APPUSER}  /home/${APPUSER}/${COMPONENT}/
 stat $?
 
 echo -n "Generating the ${COMPONENT} artifacts :"
-cd /home/${APPUSER}/${COMPONENT}
+cd /home/${APPUSER}/${COMPONENT}/
 npm install    &>> ${LOGFILE}
 stat $?
 

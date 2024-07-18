ed206d20b82d6a7e7f6055ad88ddeb68d4069b3d
sarvan <msaravanakumar312@gmail.com>
2024-07-18 09:16:09 +0530

    Updated comments

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -5,7 +5,7 @@ COMPONENT=mongodb
 LOGFILE="/tmp/${COMPONENT}.log"
 
 if [ $USER_ID -ne 0 ] ; then
-    echo -e "\e[32m Script is expected to excuted by the root user or sudo previllage \e[0m \n \t Example: sudo bash wrapper.sh mongodb"
+    echo -e "\e[32m Script is expected to excuted by the root user or sudo previllage \e[0m \n \t Example: sudo bash wrapper.sh ${COMPONENT}"
     exit 1
 fi
 
@@ -20,40 +20,35 @@ stat() {
 echo -e "\e[33m configuring ${COMPONENT}...! \e[0m \n"
 
 echo -n "Installing ${COMPONENT} repo :"    
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
+curl -s -o /etc/yum.repos.d/${COMPONENT}.repo https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/${COMPONENT}.repo
 stat $?
 
 echo -n "Installing ${COMPONENT} :"
-yum install -y mongodb-org     &>> ${LOGFILE}
+yum install -y ${COMPONENT}-org     &>> ${LOGFILE}
 stat $?
 
 echo -n "Enabling the ${COMPONENT} Visibility :"
-sed -ie 's/127.0.0.0/0.0.0.0.0/g' /etc/mongod.conf  &>> ${LOGFILE}
+sed -ie 's/127.0.0.0/0.0.0.0.0/g' /etc/${COMPONENT}.conf  &>> ${LOGFILE}
 stat $?
 
 echo -n "Starting the ${COMPONENT} :"
-systemctl enable mongod
-systemctl start mongod
+systemctl enable ${COMPONENT}
+systemctl start ${COMPONENT}
 stat $?
 
 echo -n "Downloading the ${COMPONENT} schema :"
-curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
+curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
 stat $?
 
 echo -n "Extracting the ${COMPONENT} schema :"
 cd /tmp
-unzip mongodb.zip     &>> ${LOGFILE}
+unzip -o ${COMPONENT}.zip     &>> ${LOGFILE}
 stat $?
 
 echo -n "Injecting the ${COMPONENT} schema :"
-cd mongodb-main
+cd ${COMPONENT}-main
 mongo < catalogue.js   &>> ${LOGFILE}
 mongo < users.js       &>> ${LOGFILE}
-stat $?
-
-echo -n "Restarting the ${COMPONENT} :"
-systemctl daemon-reload
-systemctl restart mongodb
 stat $?
 
 echo -e "\e[33m ${COMPONENT} Installation Is completed \e[0m \n"

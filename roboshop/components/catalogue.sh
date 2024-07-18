7e7721ad94a00e914cdd536b5e441337b3d9b3f6
sarvan <msaravanakumar312@gmail.com>
2024-07-18 11:34:30 +0530

    Updated comments

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -3,6 +3,7 @@
 USER_ID=$(id -u)
 COMPONENT=catalogue
 LOGFILE="/tmp/${COMPONENT}.log"
+APPUSER=roboshop
 
 if [ $USER_ID -ne 0 ] ; then
     echo -e "\e[32m Script is expected to excuted by the root user or sudo previllage \e[0m \n \t Example: sudo bash wrapper.sh catalogue"
@@ -26,6 +27,21 @@ stat $?
 echo -n "Installing NodeJS :"
 yum install nodejs -y     &>> ${LOGFILE}
 stat $?
+
+id ${APPUSER} 
+if [ $? -ne 0 ] ; then 
+    echo -n "Creating the application user Account :"
+    useradd roboshop
+    stat $?
+fi
+
+echo -n "Downloading the ${COMPONENT} :"
+curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+stat $?
+
+
+
+
 
 
 

58f18ccc5fdf14ffb724a6a5d1d6843e597d0526
sarvan <msaravanakumar312@gmail.com>
2024-07-18 11:00:39 +0530

    Updated comments

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -20,7 +20,7 @@ stat() {
 echo -e "\e[33m configuring ${COMPONENT}...! \e[0m \n"
 
 echo -n "Configuring ${COMPONENT} repo :"   
-curl -s -L -o https://rpm.nodesource.com/pub_16.x/nodistro/repo/nodesource-release-nodistro-1.noarch.rpm -y
+curl --silent --location https://rpm.nodesource.com/pub_16.x | sudo bash -
 stat $?
 
 echo -n "Installing NodeJS :"

b072414039d4c4b84f29faa8b3944ba6b02d168e
sarvan <msaravanakumar312@gmail.com>
2024-07-18 21:12:52 +0530

    Updated comments

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -57,7 +57,7 @@ npm install    &>> ${LOGFILE}
 stat $?
 
 echo -n "Configuring the ${COMPONENT} system file :"
-sed -ie 's/MONGO_DNSNAME/172.31.38.121/' /home/${APPUSER}/${COMPONENT}/systemd.servce
+sed -ie 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/${APPUSER}/${COMPONENT}/systemd.servce
 mv /home/${APPUSER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
 stat $?
 

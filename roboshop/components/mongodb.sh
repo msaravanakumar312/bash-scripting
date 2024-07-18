576248c0d618313848b0ebfc4b15b83d7263bd07
sarvan <msaravanakumar312@gmail.com>
2024-07-18 08:55:10 +0530

    Updated comments

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -11,23 +11,33 @@ fi
 
 stat() {
     if [ $1 -eq 0 ] ; then
-        echo -e "\e[31m Success \e[0m"
+        echo -e "\e[32m Success \e[0m"
     else
-        echo -e "\e[31m Failure \e[0m"
+        echo -e "\e[32m Failure \e[0m"
     fi
 }
 
 echo -e "\e[32m configuring ${COMPONENT}...! \e[0m \n"
 
-echo -n "Installing ${COMPONENT} repo :"     &>> ${LOGFILE}
+echo -n "Installing ${COMPONENT} repo :"    
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
 stat $?
 
-#echo -n "\e[31m Enabling the ${COMPONENT} Visibility :"
+echo -n "Installing ${COMPONENT} :"
+yum install -y mongodb-org     &>> ${LOGFILE}
+stst $?
+
+echo -n "Enabling the ${COMPONENT} Visibility :"
+sed -ie 's/127.0.0.0/0.0.0.0.0/g' /etc/mongod.conf  &>> ${LOGFILE}
+stat $?
+
+echo -n "Starting the ${COMPONENT} :"
+systemctl enable mongod
+systemctl start mongod
+stat $?
 
 
-# systemctl enable mongod
-# systemctl start mongod
+
 # systemctl restart mongod
 # curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
 

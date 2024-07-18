21f8834cb5fae5de6913576597de839c2cc0bad7
sarvan <msaravanakumar312@gmail.com>
2024-07-18 13:24:10 +0530

    Updated comments

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -42,7 +42,6 @@ stat $?
 
 echo -n "Copying the ${COMPONENT} to ${APPUSER} home directory :"
 cd /home/${APPUSER}/
-rm -rf ${COMPONENT}    &>> ${LOGFILE}
 unzip -o /tmp/${COMPONENT}.zip   &>> ${LOGFILE}
 stat $?
 
